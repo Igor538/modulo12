576117286860a3bbf75bf9855e96445a42e4dfe3
Igor <62676636+Igor538@users.noreply.github.com>
2025-12-23 21:24:41 -0300

    merge: resolver conflito entre master e feature/conflito

--- teste.py
+++ teste.py
@@ -1,5 +1,7 @@
-print("Testando CI/CD - bug corrigido")
+print("Alteração na master")
+print("Alteração na feature/conflito")
+print("CI/CD funcionando! Testes OK!")
+print("Simulando deploy automático...")
 
 def nova_funcionalidade():
     print("Nova funcionalidade funcionando!")
-
